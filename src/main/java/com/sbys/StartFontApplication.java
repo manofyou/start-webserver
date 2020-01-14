5b9e7ab5550cf773d4afb8ed9438c5c5d72c3555
jhjo15 <jhjo15@lotteon.net>
2020-01-14 10:28:25 +0900

    initial commit

---
+++ src/main/java/com/sbys/StartFontApplication.java
@@ -0,0 +1,13 @@
+package com.sbys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartFontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartFontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbs/StartKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
